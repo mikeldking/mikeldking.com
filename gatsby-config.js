005b67c650e1c76b99a5d3f70a75a4924853c0ba
Mikyo King <mikeldking@gmail.com>
2020-04-27 22:55:02 -0700

    skeleton blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,35 @@
  *
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
-
 module.exports = {
   /* Your site config here */
-  plugins: []
+  plugins: [
+    `gatsby-plugin-typescript`,
+    {
+      resolve: `gatsby-theme-blog`,
+      options: {
+        // basePath defaults to `/`
+        basePath: `/`,
+      },
+    },
+  ],
+  siteMetadata: {
+    // Used for the site title and SEO
+    title: `Adventures in code and climbing`,
+    // Used to provide alt text for your avatar
+    author: `Mikyo King`,
+    // Used for SEO
+    description: `A site where Mikyo explains his thoughts`,
+    // Used for social links in the root footer
+    social: [
+      {
+        name: `Twitter`,
+        url: `https://twitter.com/mikeldking`,
+      },
+      {
+        name: `GitHub`,
+        url: `https://github.com/mikeldking`,
+      },
+    ],
+  },
 }

b22731b0cd07b0e754f95049de65c8f603b223a5
Mikyo King <mikeldking@gmail.com>
2020-04-27 22:55:30 -0700

    shadow blog components

---
+++ src/gatsby-theme-blog/components/bio-content.tsx
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react"
+import { Styled } from "theme-ui"
+
+export default () => (
+  <Fragment>
+    Words by{" "}
+    <Styled.a href="http://twitter.com/mikeldking">Mikyo King</Styled.a>.
+    <br />
+    My thoughts as a software engineer
+  </Fragment>
+)

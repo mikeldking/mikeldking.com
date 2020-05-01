1e36569b4cd4b8d624e074fa3017db6c1e5114d7
Mikyo King <mikeldking@gmail.com>
2020-04-30 21:50:11 -0700

    Resume styles

--- src/gatsby-theme-blog/components/bio-content.tsx
+++ src/gatsby-theme-blog/components/bio-content.tsx
@@ -4,8 +4,8 @@ import { Styled } from "theme-ui"
 export default () => (
   <Fragment>
     👋 I'm <Styled.a href="http://twitter.com/mikeldking">Mikel King</Styled.a>.
-    I go by Mikyo.
+    I go by Mikyo and this is a blog about 👨🏻‍💻and 🧗🏻‍♂️
     <br />
-    This is a blog about 👨🏻‍💻 and 🧗🏻‍♂️
+    Check out my <Styled.a href="/resume">resume</Styled.a> if you feel like it
   </Fragment>
 )
